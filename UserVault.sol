e21f41c12a6bbcc40255eb4616ffd6cb37b60e8e
0x7s0lt1 <zsoltfehervari@gmx.com>
2024-08-08 13:37:19 +0200

    chunk

--- UserVault.sol
+++ UserVault.sol
@@ -2,7 +2,6 @@
 
 pragma solidity >=0.4.22 <0.9.0;
 
-import "./VaultItem.sol";
 import "./LoyalityCard.sol";
 import "./DebitCard.sol";
 import "./Password.sol";
@@ -12,51 +11,20 @@ import "./Utils.sol";
 contract UserVault is Payable{
 
 
-    mapping (uint => address[]) Items;
+    mapping (Utils.ItemType => address[]) Items;
 
-    constructor(address payable _owner, address _manager) 
-    Payable( _owner, _manager) 
-    {
-       
-    }
+    constructor(address payable _owner, address _manager) Payable( _owner, _manager) {}
 
     function getAddress() external view ownerOnly returns (address) {
 
         return address(this);
     }
 
-    function getItems( Utils.ItemType _type) public ownerOnly view returns (address[] memory) {
-
-        return Items[uint(_type)];
-    }
-
-    function putItem(address _item, Utils.ItemType _type) external ownerOnly {
-
-        uint256 idx = uint256(_type);
-        require(Utils.findIndex(_item, Items[idx]) > -1, "Exists");
-
-        Items[idx].push(_item);
-    }
-
-    function deleteItem(address _item, Utils.ItemType _type) public ownerOnly{
-
-        uint256 idx = uint256(_type);
-        int index = Utils.findIndex(_item, Items[idx]);
-        require(index > -1 && index < int(Items[idx].length), "Item not found in array");
-
-        for (uint i = uint(index); i < Items[idx].length - 1; i++) {
-            Items[idx][i] = Items[idx][i + 1];
-        }
-
-        Items[idx].pop(); 
-
-    }
-
     function createLoyalityCard(string memory _name, string memory _card_id) external ownerOnly {
 
         LoyalityCard card = new LoyalityCard( _name, _card_id, owner, address(this) );
 
-        Items[uint256(Utils.ItemType.LOYALITY_CARD)].push( card.getAddress() );
+        Items[Utils.ItemType.LOYALITY_CARD].push( card.getAddress() );
         
     }
 
@@ -70,7 +38,7 @@ contract UserVault is Payable{
 
         DebitCard card = new DebitCard( _name, _card_id, _name_on_card, _expire_at, _cvv, owner, address(this));
 
-        Items[uint256(Utils.ItemType.DEBIT_CARD)].push( card.getAddress() );
+        Items[Utils.ItemType.DEBIT_CARD].push( card.getAddress() );
         
     }
 
@@ -83,7 +51,7 @@ contract UserVault is Payable{
 
         Password psw = new Password( _name, _url, _user_name, _password, owner, address(this));
 
-        Items[uint256(Utils.ItemType.PASSWORD)].push( psw.getAddress() );
+        Items[Utils.ItemType.PASSWORD].push( psw.getAddress() );
         
     }
 
--- Utils.sol
+++ Utils.sol
@@ -2,6 +2,7 @@
 
 pragma solidity >=0.4.22 <0.9.0;
 
+import "./Payable.sol";
 
 library Utils{
 
@@ -14,6 +15,28 @@ library Utils{
             }
         }
         return -1; 
+    }
+
+    function getItems( mapping (ItemType => address[]) storage _items) public view returns (address[][3] memory) {
+
+        return [
+            _items[ItemType.LOYALITY_CARD],
+            _items[ItemType.DEBIT_CARD],
+            _items[ItemType.PASSWORD]
+        ];
+    }
+
+    function deleteItem( mapping (ItemType => address[]) storage _items, address _item, ItemType _type) public {
+
+        int index = Utils.findIndex(_item, _items[_type]);
+        require(index > -1 && index < int(_items[_type].length), "Item not found in array");
+
+        for (uint i = uint(index); i < _items[_type].length - 1; i++) {
+            _items[_type][i] = _items[_type][i + 1];
+        }
+
+        _items[_type].pop(); 
+
     }
 
 }
